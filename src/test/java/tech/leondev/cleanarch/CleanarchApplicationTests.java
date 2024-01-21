c4ab9bebf3ff878b5477cfdcae26d0c2ff81793f
leo <nascimentoleo899@gmail.com>
2024-01-21 16:14:08 -0300

    chore: first commit

---
+++ src/test/java/tech/leondev/cleanarch/CleanarchApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.leondev.cleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
